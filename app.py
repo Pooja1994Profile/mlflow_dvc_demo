c53db2615ba4e997e26b61653047746d1ac539fd
Pooja <bhatipooja94@gmail.com>
2021-09-14 14:27:26 +0530

    workflow updated:heroku

--- app.py
+++ app.py
@@ -83,6 +83,8 @@ def index():
         return render_template('index.html')
 
 
+port = int(os.getenv("PORT"))
+
 if __name__ == "__main__":
     # app.run(host='127.0.0.1', port=8001, debug=True)
     #app.run(debug=True) # running the app
